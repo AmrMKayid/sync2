80555fafcac7b16263c01663ac893bd26f70c757
amrmkayid <amrkayid2027@gmail.com>
2019-12-13 17:56:50 +0200

    New Update: 13/12/2019
    commit 0d2b0eef6bbdc36284c8d5f7bf6a970b2baa51d9
    Author: amrmkayid <amrkayid2027@gmail.com>
    Date:   Fri Dec 13 17:51:59 2019 +0200
    
        New Update: 13/12/2019

--- sync.sh
+++ sync.sh
@@ -12,19 +12,27 @@ clone_and_sync() {
     git pull $RL_PUBLIC_REPO_URL
   fi
 
-  rsync -azP -r -u $ROOT_DIRECTORY/ $RL_PUBLIC_FOLDER/
+  rsync -azP -r -u --exclude '.git' $ROOT_DIRECTORY/ $RL_PUBLIC_FOLDER/
 }
 
 new_branch_update() {
   now=$(date +'%d/%m/%Y')
+  last_commit_msg=$(
+    cd $ROOT_DIRECTORY &
+    (git log -1)
+  )
+
   cd $RL_PUBLIC_FOLDER/
   git pull $RL_PUBLIC_REPO_URL
   update_branch="update_${now}"
-  echo $update_branch
-  git checkout -b $update_branch
+
+  echo "Checkout out new branch: ${update_branch}"
+  git checkout -B $update_branch
+
   git add .
-  git commit -m "New Update: ${now}"
+  git commit -m "New Update: ${now}" -m "${last_commit_msg}"
   git push --set-upstream origin $update_branch
+
 }
 
 clone_and_sync
